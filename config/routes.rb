22cf4779f473124763fe4a2a7365d09a3d5a4d9c
rails -v <c:sites>
2017-05-26 11:55:57 -0500

    despaci
    dsds

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,6 @@ Rails.application.routes.draw do
 
   get "/users", :controller => "users", :action => "index"
   get "/users/:id", :controller => "users", :action => "show"
+  get "/my_likes", :controller => "mylikes", :action => "index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
